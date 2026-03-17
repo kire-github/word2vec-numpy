a46ff9dd55be2726dcd974b0357189bf1d62b0d4
kire <ekeducatio@gmail.com>
2026-03-17 01:34:15 +0100

    Add functions to tokenize, filter, and generate training data

---
+++ dataset.py
@@ -0,0 +1,42 @@
+import re
+from collections import Counter
+import numpy as np
+
+class Dataset:
+    def __init__(self, raw_text, window_size, word_min_frequency):        
+        self.tokens = self.tokenize(raw_text)[:100000]
+        print(self.tokens[:40])
+        self.build_vocab(word_min_frequency)
+        self.window_size = window_size
+
+    def tokenize(self, raw_text):
+        """
+        Tokenize the raw text into words, splitting on non-word characters
+        """
+        return re.findall(r'\b\w+\b', raw_text.lower())
+    
+    def build_vocab(self, word_min_frequency):
+        """
+        Create the vocabulary, mappings, and token frequencies, also filter out low frequency words
+        """
+        word_freq = Counter(self.tokens)
+
+        # Create word to index and index to word mappings
+        filtered_words = [(word, freq) for word, freq in word_freq.items() if freq >= word_min_frequency]
+        self.word_to_idx = {word: idx for idx, (word, freq) in enumerate(filtered_words)}
+        self.idx_to_word = {idx: word for word, idx in self.word_to_idx.items()}
+        self.vocab_size = len(self.word_to_idx)
+
+        # Save token indices and frequencies
+        self.token_indices = [self.word_to_idx[token] for token in self.tokens if token in self.word_to_idx]
+        self.vocab_freq = np.array([word_freq[self.idx_to_word[idx]] for idx in range(self.vocab_size)])
+    
+    def generate_pairs(self):
+        """
+        Generate the positive examples for the dataset
+        """
+        # Enumerate to make sure we don't include low frequency words in the pairs
+        for idx, token_idx in enumerate(self.token_indices):
+            for context_idx in range(max(0, idx - self.window_size), min(len(self.token_indices), idx + self.window_size + 1)):
+                if context_idx != idx:
+                    yield (token_idx, self.token_indices[context_idx])
