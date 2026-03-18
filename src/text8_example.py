6ec646fe250348fd14d66afd58a9934123b93bb6
kire <ekeducatio@gmail.com>
2026-03-18 23:08:26 +0100

    Add main guard

--- src/text8_example.py
+++ src/text8_example.py
@@ -3,35 +3,7 @@ from dataset import Dataset
 from model import SGNS
 from train import train
 
-# ----------------------
-# 1. Load and preprocess vocab
-# ----------------------
-print("Processing dataset...")
-
-with open("text8", "r") as f:
-    text = f.read()
-
-dataset = Dataset(text)
-
-print("Dataset initialized")
-
-# ----------------------
-# 2. Initialize model
-# ----------------------
-model = SGNS(vocab_size=dataset.vocab_size)
-
-# ----------------------
-# 3. Training loop
-# ----------------------
-print("Starting training...")
-train(dataset, model)
-print("Training completed")
-
-# ----------------------
-# 4. Test
-# ----------------------
-print("Testing embeddings...")
-def most_similar(word, number=5):
+def most_similar(word, dataset, model,number=5):
     if word not in dataset.word_to_idx:
         return []
     idx = dataset.word_to_idx[word]
@@ -40,7 +12,36 @@ def most_similar(word, number=5):
     nearest = sims.argsort()[-number-1:-1][::-1]
     return [dataset.idx_to_word[i] for i in nearest]
 
-# Example queries
-print("Most similar to 'anarchism':", most_similar("anarchism"))
-print("Most similar to 'term':", most_similar("term"))
-print("Most similar to 'class':", most_similar("class"))
+
+if __name__ == "__main__":
+    # ----------------------
+    # 1. Load and preprocess vocab
+    # ----------------------
+    print("Processing dataset...")
+
+    with open("text8", "r") as f:
+        text = f.read()
+
+    dataset = Dataset(text)
+
+    print("Dataset initialized")
+
+    # ----------------------
+    # 2. Initialize model
+    # ----------------------
+    model = SGNS(vocab_size=dataset.vocab_size)
+
+    # ----------------------
+    # 3. Training loop
+    # ----------------------
+    print("Starting training...")
+    train(dataset, model)
+    print("Training completed")
+
+    # ----------------------
+    # 4. Test
+    # ----------------------
+    print("Testing embeddings...")
+    print("Most similar to 'anarchism':", most_similar("anarchism", dataset, model))
+    print("Most similar to 'term':", most_similar("term", dataset, model))
+    print("Most similar to 'class':", most_similar("class", dataset, model))
